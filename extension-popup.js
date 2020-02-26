729af0c7a41fbea44b20414269be9c50f75bcb2d
Adam Patridge <patridge@gmail.com>
2020-02-25 18:47:21 -0700

    Add clipboard copy buttons

--- extension-popup.js
+++ extension-popup.js
@@ -6,23 +6,33 @@ let contentYamlGitUrlAnchor = document.getElementById("repoUrlYaml");
 let contentMarkdownGitUrlAnchor = document.getElementById("repoUrlMarkdown");
 
 let displayMetadata = async function (metadata) {
-    console.log("Content script received: " + metadata);
-
     msAuthorSpan.textContent = metadata.msAuthorMetaTagValue;
     gitHubAuthorSpan.textContent = metadata.gitHubAuthorMetaTagValue;
     msDateSpan.textContent = metadata.msDateMetaTagValue;
     contentYamlGitUrlAnchor.href = metadata.gitHubYamlLocationMaster;
     contentMarkdownGitUrlAnchor.href = metadata.gitHubMarkdownLocationMaster;
 
-    await navigator.clipboard.writeText(metadata.msAuthorMetaTagValue);
-    // window.alert(`Copied '${metadata.msAuthorMetaTagValue}' to clipboard.`);
-    // window.open(metadata.gitHubLocationMaster, "_blank");
+    // NOTE: Catching error because it will throw a DOMException ("Document is not focused.") whenever the window isn't focused and we try to copy to clipboard (e.g., debugging in dev tools).
+    await navigator.clipboard.writeText(metadata.msAuthorMetaTagValue).catch(error => console.log("Error while trying to copy to clipboard", error));
 };
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
     switch (request.method) {
         case "metadataCollected":
             await displayMetadata(request.data);
+
+            let copyButtons = [...document.getElementsByClassName("copy-field-btn")];
+            copyButtons.forEach(btn => {
+                btn.onclick = async function(element) {
+                    // Find nearest sibling `.copy-field-target` and copying its text to clipboard.
+                    let siblingCopyTargets = [...btn.parentNode.parentNode.getElementsByClassName("copy-field-target")];
+                    let copyTarget = siblingCopyTargets && siblingCopyTargets[0];
+                    let copyValue = copyTarget && copyTarget.innerText;
+                    // NOTE: Catching error because it will throw a DOMException ("Document is not focused.") whenever the window isn't focused and we try to copy to clipboard (e.g., debugging in dev tools).
+                    await navigator.clipboard.writeText(copyValue).catch(error => console.log("Error while trying to copy to clipboard", error));
+                };
+            });
+
             sendResponse(
                 {
                     result: "success"
@@ -37,8 +47,6 @@ chrome.tabs.query({ active: true, currentWindow: true },
         chrome.tabs.executeScript(
             tabs[0].id,
             {
-                //original_ref_skeleton_git_url
-                // code: 'console.log("hola!!!");', //commentMinimizer.hideOutdatedBotComments();
                 file: "get-author.js"
             }
         );
