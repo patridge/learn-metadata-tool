5246fe00b7d8a3af31d30bbb620d5d6340672360
Adam Patridge <patridge@gmail.com>
2022-09-23 17:57:39 -0600

    First several failures at getting GA to work

---
+++ js/ga/README.txt
@@ -0,0 +1,3 @@
+These JavaScript files are copied completely from Google Analytics hosting system because Manifest v3 doesn't allow external scripts anymore, regardless of CSP settings.
+
+And, even with an appropriate `integrity="..."` hash on the inline script, it also will not run.
---
+++ js/ga/track-pageview.js
@@ -0,0 +1,9 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-222135013-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = './js/ga/gtag-ua-222135013-1.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
